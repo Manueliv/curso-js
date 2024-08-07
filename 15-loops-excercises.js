c23699a47a3cc50a1aaaecf6e63ffd4483696f8d
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-07 16:11:48 -0600

    loops ejercisio 5

--- 15-loops-excercises.js
+++ 15-loops-excercises.js
@@ -30,9 +30,16 @@ for (let i = 0; i < nombres.length; i++) {
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
 let cadena = "Esta es una cadena de texto, para utilizar para contar el número de vocales"
-while (cadena === "a" || "e" || "i" || "o" || "u")[
-console.log()
-]
+let vocales = ["a", "á", "e", "é", "i", "í", "o", "ó", "u", "ú" ]
+let numeroVocales = 0
+
+for (let i of cadena.toLocaleLowerCase()) {
+    if (vocales.includes(i)){
+      numeroVocales = numeroVocales + 1
+    }
+}
+console.log(`El numero de vocales:  ${numeroVocales}`)
+
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
 
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
