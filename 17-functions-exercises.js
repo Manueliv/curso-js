c5f9df7235cceedba4a575efd876dcb70ac0c931
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-14 19:09:47 -0600

    Ejercisio 2 de funciones

--- 17-functions-exercises.js
+++ 17-functions-exercises.js
@@ -1,13 +1,29 @@
 // NOTA: Explora diferentes sintaxis de funciones para resolver los ejercicios
+
+// 1. Crea una función que reciba dos números y devuelva su suma
 function suma(a, b ) {
     console.log(a + b)
 }
 suma(3, 4)
-
-// 1. Crea una función que reciba dos números y devuelva su suma
-
+ 
 // 2. Crea una función que reciba un array de números y devuelva el mayor de ellos
+const myArray = [3, 5, 7, 1, 25, 40, 8, 3, 50]
 
+function numMayor() {
+    let result = 0
+    for (let i = 0; i < myArray.length; i++) {
+        let numero = myArray[i]
+        if (i == 0) {
+            result = numero
+        } else {
+            if(numero > result) {
+                result = numero
+            }
+        }
+    }
+    return result
+   }
+console.log(numMayor())
 // 3. Crea una función que reciba un string y devuelva el número de vocales que contiene
 
 // 4. Crea una función que reciba un array de strings y devuelva un nuevo array con las strings en mayúsculas
