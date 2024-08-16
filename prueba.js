1e7c077d0f28ea3f462033eb2c01ee48b020565d
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-15 19:37:15 -0600

    prueba ejercisio 6 de funciones

---
+++ prueba.js
@@ -0,0 +1,14 @@
+let array1 = [23, 14 , 51, 47, 55, 10, 11]
+let array2 = [ 40, 57, 47, 11, 3, 55, 8,]
+let nuevoArray = []
+
+
+function comunes() {
+    for (i = 0; i < array1.length; i++)
+        if (array1[array1.i] == array2[i]) {
+            nuevoArray.push(array2[i-1])     
+        }return nuevoArray
+
+       
+}
+console.log(comunes())
