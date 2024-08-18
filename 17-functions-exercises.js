827ef15fdf99504c5dc67791f10ffb43312b3b11
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-18 11:53:20 -0600

    Ejercisio 6 ok

--- 17-functions-exercises.js
+++ 17-functions-exercises.js
@@ -82,6 +82,7 @@ function comunes(array1, array2) {
 }
 let resultado = comunes(array1, array2)
 console.log(resultado)
+
 /* let array1 = [23, 14 , 51, 47, 55, 10, 11]
 let array2 = [ 40, 57, 47, 11, 3, 55, 8, 13]
 let nuevoArray = []
