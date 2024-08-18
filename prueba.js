cd35a743d2bc46982666d9ee2a8ac0cfa5179ebb
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-17 19:07:24 -0600

    ejercisio array numeros comunes

--- prueba.js
+++ prueba.js
@@ -1,14 +1,15 @@
 let array1 = [23, 14 , 51, 47, 55, 10, 11]
 let array2 = [ 40, 57, 47, 11, 3, 55, 8,]
-let nuevoArray = []
 
 
-function comunes() {
-    for (i = 0; i < array1.length; i++)
-        if (array1[array1.i] == array2[i]) {
-            nuevoArray.push(array2[i-1])     
-        }return nuevoArray
-
-       
+function comunes(array1, array2) {
+    let nuevoArray = []
+    for (i = 0; i < array1.length; i++){
+        if (array2.indexOf(array1[i]) !==-1 && nuevoArray.indexOf(array1[1])) {
+            nuevoArray.push(array1[i])     
+        }
+    }
+    return nuevoArray
 }
-console.log(comunes())
+let resultado = comunes(array1, array2)
+console.log(resultado)
