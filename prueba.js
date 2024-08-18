c0660dc05d07794ec3c8e9cc08c18fc560eb1ded
Manuel Solorzano <manueliv2008@hotmail.com>
2024-08-18 11:37:21 -0600

    prueba

--- prueba.js
+++ prueba.js
@@ -1,7 +1,6 @@
 let array1 = [23, 14 , 51, 47, 55, 10, 11]
 let array2 = [ 40, 57, 47, 11, 3, 55, 8,]
 
-
 function comunes(array1, array2) {
     let nuevoArray = []
     for (i = 0; i < array1.length; i++){
