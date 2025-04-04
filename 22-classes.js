44108b005bb204b0a5774636efe176cac1947e1e
Manuel Solorzano <manueliv2008@hotmail.com>
2025-04-04 16:17:40 -0600

    ejemplo de clases

---
+++ 22-classes.js
@@ -0,0 +1,170 @@
+// clases 
+class Person { 
+      constructor(name, age, alias) {
+        this.name = name
+        this.age = age
+        this.alias = alias
+      }
+} 
+
+//sintaxis
+
+let person = new Person("Manuel", 36, "Manueldev")
+let person2 = new Person("Manuel", 36, "Manueldev")
+
+console.log(person)
+console.log(person2)
+
+console.log(typeof person)
+
+//valores por defecto 
+
+class DefaulPerson  {
+    constructor(name = "Sin nombre", age = 0, alias = "sin alias") {
+        this.name = name
+        this.age = age
+        this.alias = alias
+    }
+}
+
+let person3 = new DefaulPerson("Manuel", 36)
+
+console.log(person3)
+
+//acceso a propiedades
+
+console.log(person3.alias)
+console.log(person3["alias"])
+
+person3.alias = "ManuelDev"
+
+console.log(person3.alias)
+
+//Funciones en clases
+
+class PersonWithMethod {
+  constructor(name, age, alias){
+    this.name = name
+    this.age = age
+    this.alias = alias
+  }
+  walk(){
+    console.log("La persona camina")
+  }
+}
+
+let person4 = new PersonWithMethod("Manuel", 36, "ManuelDev")
+person4.walk()
+
+// Propiedades privadas
+
+class PrivatePerson {
+
+  #bank
+
+  constructor(name, age, alias, bank ) {
+    this.name = name
+    this.age = age
+    this.alias = alias
+    this.#bank = bank
+  }
+
+  pay() {
+    this.#bank
+  }
+}
+
+let person5 = new PrivatePerson("Manuel", 36, "ManuelDev", "BA1234567")
+
+// No podemos acceder
+// console.log(person5.bank)
+//person5.bank = "new BA1234567"  // bank no es #bank
+
+console.log(person5)
+
+// Getters y setters
+
+class GetSetPerson {
+
+  #name
+  #age
+  #alias
+  #bank
+
+  constructor(name, age, alias, bank) {
+    this.#name = name
+    this.#age = age
+    this.#alias = alias
+    this.#bank = bank
+  }
+
+  get name() {
+    return this.#name
+  }
+
+  set bank(bank) {
+    this.#bank = bank
+  }
+}
+
+person6 = new GetSetPerson("Manuel", 36, "ManuelDev", "BA1234567")
+
+console.log(person6)
+console.log(person6.name)
+
+person6.bank = "BA1234567"
+
+// Herencia 
+class Animal {
+
+  constructor(name) {
+    this.name = name
+  }
+
+  sound() {
+    console.log("El animal emite un sonido genérico")
+  }
+}
+
+class Dog extends Animal {
+
+  sound() {
+    console.log("Guau!")
+  }
+
+  run() {
+    console.log("El perro corre")
+  }
+}
+
+class Fish extends Animal {
+
+  constructor(name, size) {
+    super(name)
+    this.size = size
+  }
+
+  swim() {
+    console.log("El pez nada")
+  }
+}
+
+let myDog = new Dog("ManuDog")
+myDog.run()
+myDog.sound()
+
+let myFish = new  Fish("ManuFish", 10)
+myFish.swim()
+myFish.sound()
+
+
+//Métodos estaticos
+
+class MathOperations {
+
+  static sum(a, b) {
+    return a + b
+  }
+}
+
+console.log(MathOperations.sum(5, 10))
