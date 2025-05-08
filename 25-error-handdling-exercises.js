62ee23e48d939fcdfa4f81bf8593b726af63a3f7
Manuel Solorzano <manueliv2008@hotmail.com>
2025-05-08 17:11:52 -0600

    Ejercicio 5 de exepciones

--- 25-error-handdling-exercises.js
+++ 25-error-handdling-exercises.js
@@ -14,15 +14,38 @@ try {
 try {
     console.log(myObject.email)
 } catch (error) {
-    console.log("Se ha producido un error:", error.message)
+    console.log("Se ha producido un error2:", error.message)
 }finally {
-    console.log("Este código se ejecuta siempre")
+    console.log("Este código se ejecuta siempre") //El código del fina se ejecuta siempre 
 }
 
 
 // 3. Lanza una excepción genérica
 
-throw new Error("Se ha producido un nuevo error")
+
+function sumIntegers(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") {
+        throw new TypeError("Esta operación sólo suma números")
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        throw new Error("Esta operación solo suma numeros enteres")
+    }
+    if (a == 0 || b == 0){
+        throw new SumZeroIntegerError("Se esta intentado sumar cero", a, b)
+    }
+    return a + b
+}
+
+try {
+    console.log(sumIntegers(5, 10))
+    //console.log(sumIntegers(5.5, 10))
+    console.log(sumIntegers("5", 10))
+    //console.log(sumIntegers(5, "10"))
+    console.log(sumIntegers("5", "10"))
+} catch (error) {
+    console.log("Se ha producido un error:", error.message)
+}
+
 
 
 // 4. Crea una excepción personalizada
@@ -39,16 +62,14 @@ class SumZeroIntegerError extends Error {
     }
 }
 
+
+// 5. Lanza una excepción personalizada
+
 try {
     console.log(sumIntegers(0, 10))
 } catch (error) {
     console.log("Se ha producido un error personalizado:", error.message)
     error.printNumbers()
-}
-
-// 5. Lanza una excepción personalizada
-
-
 
 // 6. Lanza varias excepciones según una lógica definida
 
