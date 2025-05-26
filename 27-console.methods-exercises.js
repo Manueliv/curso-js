7ebf334fb70c38d0c8c8092ab62eace14a15e72b
Manuel Solorzano <manueliv2008@hotmail.com>
2025-05-26 17:33:11 -0600

    console ejercicios

---
+++ 27-console.methods-exercises.js
@@ -0,0 +1,41 @@
+// 1. Crea un función que utilice error correctamente
+
+function funcError() {
+    console.error("Este es un mensaje utilizando console.error")
+}
+funcError()
+
+// 2. Crea una función que utilice warn correctamente
+
+function funcWarn() {
+    console.warn("Mensaje tulizando console.warn")
+}
+funcWarn()
+
+// 3. Crea una función que utilice info correctamente
+
+function funcInfo() {
+    console.info("Mensaje utilizando console.info en una función")
+}
+funcInfo()
+
+// 4. Utiliza table
+
+let pcs = [
+    {name: "Pc escritorio", procesador: "g730"},
+    {name: "Laptop hp", procesador: "i3500"},
+]
+ 
+console.table(pcs)
+
+// 5. Utiliza group
+
+// 6. Utiliza time
+
+// 7. Valida con assert si un número es positivo
+
+// 8. Utiliza count
+
+// 9. Utiliza trace
+
+// 10. Utiliza clear
