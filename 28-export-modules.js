12467fb672d91cbbb6fee9bf54ccdfa8d5b2fa6c
Manuel Solorzano <manueliv2008@hotmail.com>
2025-10-12 19:54:57 -0600

    Ejercicio 28 modulos

--- 28-export-modules.js
+++ 28-export-modules.js
@@ -9,3 +9,38 @@ export function add(a, b) {
 console.log(add(5, 10))
 
 //propiedades 
+
+
+export const PI = 3.1416
+export let name = "ManuelDev"
+// Clases
+
+export class Circle {
+
+    constructor(radius) {
+        this.radius
+    }
+
+    area() {
+    return Math.PI * Math.pow(this.radius, 2)
+}
+
+perimeter() {
+    return 2 * Math.PI * this.radius
+}
+
+}
+
+// Exportación por defecto 
+
+// export default function substract(a, b) {
+//     return a -b
+// }
+
+// export default class MyClass {
+
+//     func() {
+//         console.log("Mi clase")
+//     }
+// }
+
